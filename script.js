943ac03c2c31d5a690d8f2d07331a600d6ad47af
Ayush <ayush.6678@gmail.com>
2022-10-27 20:56:06 +0530

    Added buttons to toggle code

---
+++ script.js
@@ -0,0 +1,19 @@
+function fun() {
+    var x = document.getElementById("div1")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function fun2()
+  {
+  var x1 = document.getElementById("div2")
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  } else {
+    x1.style.display = "none";
+  }
+}
